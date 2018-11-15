6ca287e321f43cd58fa5756d31ab89eb903a73ef
Bobby Powers <bobbypowers@gmail.com>
2018-11-14 23:07:30 -0800

    ruby 2.5.2, bpowers

--- packer/discourse_install_rvm.sh
+++ packer/discourse_install_rvm.sh
@@ -1,5 +1,5 @@
 set -e
 
-rvm install 2.4.1
-rvm --default use 2.4.1 # If this error out check https://rvm.io/integration/gnome-terminal
+rvm install 2.5.2
+rvm --default use 2.5.2 # If this error out check https://rvm.io/integration/gnome-terminal
 gem install bundler mailcatcher
