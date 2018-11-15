693dd109391c134d4f063bfa9b2bbf2cfe050996
Bobby Powers <bobbypowers@gmail.com>
2018-11-14 20:04:03 -0800

    docker/bench_start.sh: ensure rbenv shims are on $PATH

--- docker/bench_start.sh
+++ docker/bench_start.sh
@@ -1,1 +1,1 @@
-sudo -H -u discourse bash -c "cd /var/rails_ruby_bench && ./start.rb --no-startup-shutdown -p 3000 \"$@\""
+sudo -H -u discourse bash -c "PATH=/home/discourse/.rbenv/shims:$PATH cd /var/rails_ruby_bench && ./start.rb --no-startup-shutdown -p 3000 \"$@\""
